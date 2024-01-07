c7bb3f8b394c27cc4b1d794ce08be8dea90d4f83
Rauibert <rauibertnf@gmail.com>
2024-01-07 20:58:32 +0100

    Svelte structure

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Formulario from "./formulario.svelte";
+  let name = "Svelte";
+  let cod_feature = "";
+</script>
+
+
+
+
+<h1>Feature Generator Svelte</h1>
+
+<h2>Hola {name}</h2>
+<div class="formulario">
+  <Formulario />
+</div>
+
+
+<main>
+  Feature: {cod_feature}
+  
+</main>
+
+
+<style>
+.formulario{
+    width: 80%;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/routes/formulario.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+<form action="">
+  <label for="GET-cod">Código del proyecto:</label>
+  <input id="GET-cod" type="text" name="cod" /><br/>
+
+  <label for="GET-name">Nombre de la feature:</label>
+  <input id="GET-name" type="text" name="name" /><br/>
+
+  <label for="GET-name">:</label>
+  <input id="GET-name" type="text" name="name" /><br/>
+
+  <label for="GET-name">Nombre:</label>
+  <input id="GET-name" type="text" name="name" /><br/>
+
+
+
+
+  <input type="submit" value="Save" />
+</form>
+
+<style>
+
+</style>
